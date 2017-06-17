db0de5f4c5ab98b91aa27a126e5f92099e486cd4
Sergey8554 <sergey8554@yandex.ru>
2017-06-17 21:43:22 +0300

    Исправление ошибок.

--- CellularAutomaton.Form/Components/Recorder.Designer.cs
+++ CellularAutomaton.Form/Components/Recorder.Designer.cs
@@ -28,46 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.GroupBox gBSettings;
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Recorder));
-            System.Windows.Forms.TableLayoutPanel tLPSettings;
-            System.Windows.Forms.GroupBox gBSettingsField;
-            System.Windows.Forms.TableLayoutPanel tLPSettingsField;
-            System.Windows.Forms.Label lHeight;
-            System.Windows.Forms.Label lWidth;
-            System.Windows.Forms.GroupBox gBSettingsCellularAutomaton;
-            System.Windows.Forms.TableLayoutPanel tLPSettingsCellularAutomaton;
-            System.Windows.Forms.Label lRule;
-            System.Windows.Forms.Label lDencity;
-            System.Windows.Forms.Label lStatesCount;
+            this.gBSettings = new System.Windows.Forms.GroupBox();
+            this.tLPSettings = new System.Windows.Forms.TableLayoutPanel();
+            this.gBSettingsField = new System.Windows.Forms.GroupBox();
+            this.tLPSettingsField = new System.Windows.Forms.TableLayoutPanel();
+            this.lHeight = new System.Windows.Forms.Label();
+            this.lWidth = new System.Windows.Forms.Label();
             this.nUDHeight = new System.Windows.Forms.NumericUpDown();
             this.nUDWidth = new System.Windows.Forms.NumericUpDown();
+            this.gBSettingsCellularAutomaton = new System.Windows.Forms.GroupBox();
+            this.tLPSettingsCellularAutomaton = new System.Windows.Forms.TableLayoutPanel();
+            this.lRule = new System.Windows.Forms.Label();
             this.cBCellularAutomatonRules = new System.Windows.Forms.ComboBox();
             this.nUDStatesCount = new System.Windows.Forms.NumericUpDown();
+            this.lDencity = new System.Windows.Forms.Label();
             this.nUDDencity = new System.Windows.Forms.NumericUpDown();
+            this.lStatesCount = new System.Windows.Forms.Label();
             this.tLPMain = new System.Windows.Forms.TableLayoutPanel();
             this.bStop = new System.Windows.Forms.Button();
             this.bRecord = new System.Windows.Forms.Button();
             this.bSave = new System.Windows.Forms.Button();
-            gBSettings = new System.Windows.Forms.GroupBox();
-            tLPSettings = new System.Windows.Forms.TableLayoutPanel();
-            gBSettingsField = new System.Windows.Forms.GroupBox();
-            tLPSettingsField = new System.Windows.Forms.TableLayoutPanel();
-            lHeight = new System.Windows.Forms.Label();
-            lWidth = new System.Windows.Forms.Label();
-            gBSettingsCellularAutomaton = new System.Windows.Forms.GroupBox();
-            tLPSettingsCellularAutomaton = new System.Windows.Forms.TableLayoutPanel();
-            lRule = new System.Windows.Forms.Label();
-            lDencity = new System.Windows.Forms.Label();
-            lStatesCount = new System.Windows.Forms.Label();
-            gBSettings.SuspendLayout();
-            tLPSettings.SuspendLayout();
-            gBSettingsField.SuspendLayout();
-            tLPSettingsField.SuspendLayout();
+            this.gBSettings.SuspendLayout();
+            this.tLPSettings.SuspendLayout();
+            this.gBSettingsField.SuspendLayout();
+            this.tLPSettingsField.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.nUDHeight)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.nUDWidth)).BeginInit();
-            gBSettingsCellularAutomaton.SuspendLayout();
-            tLPSettingsCellularAutomaton.SuspendLayout();
+            this.gBSettingsCellularAutomaton.SuspendLayout();
+            this.tLPSettingsCellularAutomaton.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.nUDStatesCount)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.nUDDencity)).BeginInit();
             this.tLPMain.SuspendLayout();
@@ -75,44 +64,44 @@
             // 
             // gBSettings
             // 
-            resources.ApplyResources(gBSettings, "gBSettings");
-            this.tLPMain.SetColumnSpan(gBSettings, 2);
-            gBSettings.Controls.Add(tLPSettings);
-            gBSettings.Name = "gBSettings";
-            gBSettings.TabStop = false;
+            resources.ApplyResources(this.gBSettings, "gBSettings");
+            this.tLPMain.SetColumnSpan(this.gBSettings, 2);
+            this.gBSettings.Controls.Add(this.tLPSettings);
+            this.gBSettings.Name = "gBSettings";
+            this.gBSettings.TabStop = false;
             // 
             // tLPSettings
             // 
-            resources.ApplyResources(tLPSettings, "tLPSettings");
-            tLPSettings.Controls.Add(gBSettingsField, 0, 0);
-            tLPSettings.Controls.Add(gBSettingsCellularAutomaton, 0, 1);
-            tLPSettings.Name = "tLPSettings";
+            resources.ApplyResources(this.tLPSettings, "tLPSettings");
+            this.tLPSettings.Controls.Add(this.gBSettingsField, 0, 0);
+            this.tLPSettings.Controls.Add(this.gBSettingsCellularAutomaton, 0, 1);
+            this.tLPSettings.Name = "tLPSettings";
             // 
             // gBSettingsField
             // 
-            resources.ApplyResources(gBSettingsField, "gBSettingsField");
-            gBSettingsField.Controls.Add(tLPSettingsField);
-            gBSettingsField.Name = "gBSettingsField";
-            gBSettingsField.TabStop = false;
+            resources.ApplyResources(this.gBSettingsField, "gBSettingsField");
+            this.gBSettingsField.Controls.Add(this.tLPSettingsField);
+            this.gBSettingsField.Name = "gBSettingsField";
+            this.gBSettingsField.TabStop = false;
             // 
             // tLPSettingsField
             // 
-            resources.ApplyResources(tLPSettingsField, "tLPSettingsField");
-            tLPSettingsField.Controls.Add(lHeight, 0, 1);
-            tLPSettingsField.Controls.Add(lWidth, 0, 0);
-            tLPSettingsField.Controls.Add(this.nUDHeight, 1, 1);
-            tLPSettingsField.Controls.Add(this.nUDWidth, 1, 0);
-            tLPSettingsField.Name = "tLPSettingsField";
+            resources.ApplyResources(this.tLPSettingsField, "tLPSettingsField");
+            this.tLPSettingsField.Controls.Add(this.lHeight, 0, 1);
+            this.tLPSettingsField.Controls.Add(this.lWidth, 0, 0);
+            this.tLPSettingsField.Controls.Add(this.nUDHeight, 1, 1);
+            this.tLPSettingsField.Controls.Add(this.nUDWidth, 1, 0);
+            this.tLPSettingsField.Name = "tLPSettingsField";
             // 
             // lHeight
             // 
-            resources.ApplyResources(lHeight, "lHeight");
-            lHeight.Name = "lHeight";
+            resources.ApplyResources(this.lHeight, "lHeight");
+            this.lHeight.Name = "lHeight";
             // 
             // lWidth
             // 
-            resources.ApplyResources(lWidth, "lWidth");
-            lWidth.Name = "lWidth";
+            resources.ApplyResources(this.lWidth, "lWidth");
+            this.lWidth.Name = "lWidth";
             // 
             // nUDHeight
             // 
@@ -156,26 +145,26 @@
             // 
             // gBSettingsCellularAutomaton
             // 
-            resources.ApplyResources(gBSettingsCellularAutomaton, "gBSettingsCellularAutomaton");
-            gBSettingsCellularAutomaton.Controls.Add(tLPSettingsCellularAutomaton);
-            gBSettingsCellularAutomaton.Name = "gBSettingsCellularAutomaton";
-            gBSettingsCellularAutomaton.TabStop = false;
+            resources.ApplyResources(this.gBSettingsCellularAutomaton, "gBSettingsCellularAutomaton");
+            this.gBSettingsCellularAutomaton.Controls.Add(this.tLPSettingsCellularAutomaton);
+            this.gBSettingsCellularAutomaton.Name = "gBSettingsCellularAutomaton";
+            this.gBSettingsCellularAutomaton.TabStop = false;
             // 
             // tLPSettingsCellularAutomaton
             // 
-            resources.ApplyResources(tLPSettingsCellularAutomaton, "tLPSettingsCellularAutomaton");
-            tLPSettingsCellularAutomaton.Controls.Add(lRule, 0, 2);
-            tLPSettingsCellularAutomaton.Controls.Add(this.cBCellularAutomatonRules, 1, 2);
-            tLPSettingsCellularAutomaton.Controls.Add(this.nUDStatesCount, 1, 1);
-            tLPSettingsCellularAutomaton.Controls.Add(lDencity, 0, 0);
-            tLPSettingsCellularAutomaton.Controls.Add(this.nUDDencity, 1, 0);
-            tLPSettingsCellularAutomaton.Controls.Add(lStatesCount, 0, 1);
-            tLPSettingsCellularAutomaton.Name = "tLPSettingsCellularAutomaton";
+            resources.ApplyResources(this.tLPSettingsCellularAutomaton, "tLPSettingsCellularAutomaton");
+            this.tLPSettingsCellularAutomaton.Controls.Add(this.lRule, 0, 2);
+            this.tLPSettingsCellularAutomaton.Controls.Add(this.cBCellularAutomatonRules, 1, 2);
+            this.tLPSettingsCellularAutomaton.Controls.Add(this.nUDStatesCount, 1, 1);
+            this.tLPSettingsCellularAutomaton.Controls.Add(this.lDencity, 0, 0);
+            this.tLPSettingsCellularAutomaton.Controls.Add(this.nUDDencity, 1, 0);
+            this.tLPSettingsCellularAutomaton.Controls.Add(this.lStatesCount, 0, 1);
+            this.tLPSettingsCellularAutomaton.Name = "tLPSettingsCellularAutomaton";
             // 
             // lRule
             // 
-            resources.ApplyResources(lRule, "lRule");
-            lRule.Name = "lRule";
+            resources.ApplyResources(this.lRule, "lRule");
+            this.lRule.Name = "lRule";
             // 
             // cBCellularAutomatonRules
             // 
@@ -191,8 +180,8 @@
             // 
             // lDencity
             // 
-            resources.ApplyResources(lDencity, "lDencity");
-            lDencity.Name = "lDencity";
+            resources.ApplyResources(this.lDencity, "lDencity");
+            this.lDencity.Name = "lDencity";
             // 
             // nUDDencity
             // 
@@ -201,13 +190,13 @@
             // 
             // lStatesCount
             // 
-            resources.ApplyResources(lStatesCount, "lStatesCount");
-            lStatesCount.Name = "lStatesCount";
+            resources.ApplyResources(this.lStatesCount, "lStatesCount");
+            this.lStatesCount.Name = "lStatesCount";
             // 
             // tLPMain
             // 
             resources.ApplyResources(this.tLPMain, "tLPMain");
-            this.tLPMain.Controls.Add(gBSettings, 0, 0);
+            this.tLPMain.Controls.Add(this.gBSettings, 0, 0);
             this.tLPMain.Controls.Add(this.bStop, 1, 1);
             this.tLPMain.Controls.Add(this.bRecord, 0, 1);
             this.tLPMain.Controls.Add(this.bSave, 0, 2);
@@ -241,20 +230,20 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.tLPMain);
             this.Name = "Recorder";
-            gBSettings.ResumeLayout(false);
-            gBSettings.PerformLayout();
-            tLPSettings.ResumeLayout(false);
-            tLPSettings.PerformLayout();
-            gBSettingsField.ResumeLayout(false);
-            gBSettingsField.PerformLayout();
-            tLPSettingsField.ResumeLayout(false);
-            tLPSettingsField.PerformLayout();
+            this.gBSettings.ResumeLayout(false);
+            this.gBSettings.PerformLayout();
+            this.tLPSettings.ResumeLayout(false);
+            this.tLPSettings.PerformLayout();
+            this.gBSettingsField.ResumeLayout(false);
+            this.gBSettingsField.PerformLayout();
+            this.tLPSettingsField.ResumeLayout(false);
+            this.tLPSettingsField.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.nUDHeight)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.nUDWidth)).EndInit();
-            gBSettingsCellularAutomaton.ResumeLayout(false);
-            gBSettingsCellularAutomaton.PerformLayout();
-            tLPSettingsCellularAutomaton.ResumeLayout(false);
-            tLPSettingsCellularAutomaton.PerformLayout();
+            this.gBSettingsCellularAutomaton.ResumeLayout(false);
+            this.gBSettingsCellularAutomaton.PerformLayout();
+            this.tLPSettingsCellularAutomaton.ResumeLayout(false);
+            this.tLPSettingsCellularAutomaton.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.nUDStatesCount)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.nUDDencity)).EndInit();
             this.tLPMain.ResumeLayout(false);
@@ -275,5 +264,16 @@
         private System.Windows.Forms.Button bStop;
         private System.Windows.Forms.Button bRecord;
         private System.Windows.Forms.Button bSave;
+        private System.Windows.Forms.GroupBox gBSettings;
+        private System.Windows.Forms.TableLayoutPanel tLPSettings;
+        private System.Windows.Forms.GroupBox gBSettingsField;
+        private System.Windows.Forms.TableLayoutPanel tLPSettingsField;
+        private System.Windows.Forms.Label lHeight;
+        private System.Windows.Forms.Label lWidth;
+        private System.Windows.Forms.GroupBox gBSettingsCellularAutomaton;
+        private System.Windows.Forms.TableLayoutPanel tLPSettingsCellularAutomaton;
+        private System.Windows.Forms.Label lRule;
+        private System.Windows.Forms.Label lDencity;
+        private System.Windows.Forms.Label lStatesCount;
     }
 }
