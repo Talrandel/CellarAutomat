56a640d4ff24b5f48cf964fdfe8c31dbf4ba34c6
Sergey8554 <sergey8554@yandex.ru>
2017-06-17 23:33:51 +0300

    Добавил свойства на SizeFieldHeightValue и SizeFieldWidthValue.

--- CellularAutomaton.Form/Components/Recorder.cs
+++ CellularAutomaton.Form/Components/Recorder.cs
@@ -6,7 +6,7 @@
 // File          : Recorder.cs
 // Author        : Антипкин С.С., Макаров Е.А.
 // Created       : 17.06.2017 20:30
-// Last Revision : 17.06.2017 23:15
+// Last Revision : 17.06.2017 23:21
 // Description   : 
 #endregion
 
@@ -73,6 +73,32 @@ namespace CellarAutomatForm.Components
         }
 
         /// <summary>
+        /// Возвращает или задаёт ширину поля клеточного автомата.
+        /// </summary>
+        /// <exception cref="ArgumentOutOfRangeException">'<see cref="SizeFieldHeightValue"/>' должно лежать в диапазоне от '<see cref="SizeFieldHeightMin"/>' до '<see cref="SizeFieldHeightMax"/>'.</exception>
+        [Browsable(true)]
+        [EditorBrowsable(EditorBrowsableState.Always)]
+        [SRCategory("SizeFieldWidth")]
+        [SRDescription(nameof(SizeFieldWidthValue) + SRDescriptionAttribute.Suffix)]
+        public short SizeFieldWidthValue
+        {
+            get { return Convert.ToInt16(nUDWidth.Value); }
+            set
+            {
+                if (value < SizeFieldWidthMin ||
+                    SizeFieldWidthMax < value)
+                {
+                    throw new ArgumentOutOfRangeException(
+                          nameof(SizeFieldWidthValue),
+                          value,
+                          $"'{nameof(SizeFieldWidthValue)}' должно лежать в диапазоне от '{nameof(SizeFieldWidthMin)}' до '{nameof(SizeFieldWidthMax)}'.");
+                }
+
+                nUDWidth.Value = value;
+            }
+        }
+
+        /// <summary>
         /// Возвращает или задаёт минимальную высоту поля клеточного автомата.
         /// </summary>
         [Browsable(true)]
@@ -114,6 +140,30 @@ namespace CellarAutomatForm.Components
 
                 nUDHeight.Maximum = value;
             }
+        }
+
+        /// <summary>
+        /// Возвращает или задаёт высоту поля клеточного автомата.
+        /// </summary>
+        /// <exception cref="ArgumentOutOfRangeException">'<see cref="SizeFieldHeightValue"/>' должно лежать в диапазоне от '<see cref="SizeFieldHeightMin"/>' до '<see cref="SizeFieldHeightMax"/>'.</exception>
+        [Browsable(true)]
+        [EditorBrowsable(EditorBrowsableState.Always)]
+        [SRCategory("SizeFieldHeight")]
+        [SRDescription(nameof(SizeFieldHeightValue) + SRDescriptionAttribute.Suffix)]
+        public short SizeFieldHeightValue
+        {
+            get { return Convert.ToInt16(nUDHeight.Value); }
+            set {
+                if (value < SizeFieldHeightMin ||
+                    SizeFieldHeightMax < value)
+                {
+                    throw new ArgumentOutOfRangeException(
+                          nameof(SizeFieldHeightValue),
+                          value,
+                          $"'{nameof(SizeFieldHeightValue)}' должно лежать в диапазоне от '{nameof(SizeFieldHeightMin)}' до '{nameof(SizeFieldHeightMax)}'.");
+                }
+
+                nUDHeight.Value = value; }
         }
 
         /// <summary>
